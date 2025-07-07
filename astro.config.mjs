b2903e52a685f4bc63072efe5f9807ccd85a64a5
nicoCorbal <nicocorbal@gmail.com>
2025-07-07 17:43:31 +0200

    fix: remove unused preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import preact from '@astrojs/preact';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://OSIX-Tech.github.io',
   base: '/Opillsv2',
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind()]
 });
