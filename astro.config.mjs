49936ee269ac289857eaafb9d22c4b363cdfb60e
nicoCorbal <nicocorbal@gmail.com>
2025-07-13 19:31:09 +0200

    v10201

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://OSIX-Tech.github.io',
   base: '/Opillsv2',
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
---
+++ src/lib/navLinks.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { href: '#page-top', text: 'Home' },
+  { href: '#features', text: 'Features' },
+  { href: '#about', text: 'About' },
+  { href: '#testimonials', text: 'Reviews' },
+]; 
